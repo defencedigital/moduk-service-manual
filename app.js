5c78c032b3c93a5f2c3109efbfb4baedd4fa8094
trevorsaint <hello@trevorsaint.uk>
2022-08-24 14:12:04 +0100

    Improved structure, layouts, macros and partials

--- app.js
+++ app.js
@@ -56,7 +56,9 @@ app.set('view engine', 'njk');
 // Nunjucks configuration
 const appViews = [
   path.join(__dirname, '/app/views'),
-  path.join(__dirname, '/app/views/components'),
+  path.join(__dirname, '/app/layouts'),
+  path.join(__dirname, '/app/components'),
+  path.join(__dirname, '/app/partials'),
   path.join(__dirname, '/node_modules/govuk-frontend')
 ];
 
