9a38af73f0eea2e8042d76f60c7ad68783d0a69d
Trevor Saint <trevorsaint@users.noreply.github.com>
2023-01-16 15:46:30 +0000

    Code housekeeping

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const { series, parallel } = require('gulp');
 
-
 // Pull in each task
 const clean   = require('./gulp-tasks/clean.js');
 const styles  = require('./gulp-tasks/styles.js');
@@ -8,18 +7,14 @@ const scripts = require('./gulp-tasks/scripts.js');
 const images  = require('./gulp-tasks/images.js');
 const html    = require('./gulp-tasks/html.js');
 
-
 // Minify HTML
 exports.minify = series(html);
-
 
 // Clean public folder
 exports.clean = series(clean);
 
-
 // Clean public folder then run each task in parallel
 exports.build = series(clean, parallel(styles, scripts, images));
-
 
 // Run each task in parallel ready for production
 exports.prod = parallel(styles, scripts, images, html);
